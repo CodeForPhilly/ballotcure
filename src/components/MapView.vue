444a18b3d83e96e26c229fb067b4b85d7d8c44fb
Chris Alfano <chris@jarv.us>
2024-11-10 21:51:28 -0500

    feat: load divisions onto map

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -16,6 +16,58 @@ onMounted(() => {
 
   // Add navigation controls
   map.addControl(new maplibregl.NavigationControl())
+
+  // Add GeoJSON data after map loads
+  map.on('load', () => {
+    map.addSource('divisions', {
+      type: 'geojson',
+      data: '/data/philadelphia-divisions.geojson'
+    })
+
+    map.addLayer({
+      id: 'divisions-layer',
+      type: 'line',  // Changed to line type for better visibility
+      source: 'divisions',
+      paint: {
+        'line-color': '#627BC1',
+        'line-width': 1.5
+      }
+    })
+
+    // Add fill layer below the line layer
+    map.addLayer({
+      id: 'divisions-fill',
+      type: 'fill',
+      source: 'divisions',
+      paint: {
+        'fill-color': '#627BC1',
+        'fill-opacity': 0.2
+      }
+    }, 'divisions-layer')  // Insert below the line layer
+
+    // Add hover effect
+    map.addLayer({
+      id: 'divisions-hover',
+      type: 'line',
+      source: 'divisions',
+      paint: {
+        'line-color': '#000000',
+        'line-width': 2
+      },
+      filter: ['==', ['get', 'division'], '']
+    })
+
+    // Add hover interaction
+    map.on('mousemove', 'divisions-layer', (e) => {
+      if (e.features.length > 0) {
+        map.setFilter('divisions-hover', ['==', ['get', 'division'], e.features[0].properties.division])
+      }
+    })
+
+    map.on('mouseleave', 'divisions-layer', () => {
+      map.setFilter('divisions-hover', ['==', ['get', 'division'], ''])
+    })
+  })
 })
 
 onUnmounted(() => {
