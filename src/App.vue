af76857cc729cfef8f892da81e9ff7a7df6831d2
Chris Alfano <chris@jarv.us>
2024-11-10 21:05:30 -0500

    fix: maximize viewport

--- src/App.vue
+++ src/App.vue
@@ -20,20 +20,42 @@ function handleSearch(query) {
 </template>
 
 <style>
+/* Reset default margins and ensure full viewport coverage */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 html, body {
   margin: 0;
   padding: 0;
   height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
 }
 
 .app {
   display: flex;
   flex-direction: column;
   height: 100vh;
+  width: 100vw;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 
 .map-wrapper {
   flex: 1;
   min-height: 0; /* Important for proper flexbox behavior */
+  width: 100%;
+  position: relative;
 }
 </style>
